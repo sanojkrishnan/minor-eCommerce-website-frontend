26f3f3c405e7fbb1fb080be9da804cbdd9886c1d
sanojkrishnan <sanojkrishnan12@gmail.com>
2025-12-24 20:23:26 +0530

    third push

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import NavBar from './Component/NavBar'
+import React from "react";
+import NavBar from "./Component/NavBar";
+import Home from "./Component/Home";
+import { Route, Routes } from "react-router-dom";
+import NotFound from "./Component/404";
 
 function App() {
   return (
     <>
-    <NavBar />
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Card from './Card'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4'>
+      <Card />
+  
+    </div>
   )
 }
 
